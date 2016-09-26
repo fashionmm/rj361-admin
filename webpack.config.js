bb93337085c262346169a63e22689ffe523ce378
fashionmm <fashionmm@126.com>
2016-09-26 17:03:02 +0800

    'metronic模板'

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var webpack=require("webpack");
+
+module.exports={
+    entry:{
+        index:"./src/main.js"
+    },
+    output:{
+        path:"./static",
+        filename:"bundle.js"
+    }
+
+}
